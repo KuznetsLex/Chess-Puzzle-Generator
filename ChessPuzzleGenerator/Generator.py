7cd41bf85117a53a0ed8c0661e74e0299a4d38fd
KuznetsLex <kuznets.lex@gmail.com>
2024-07-22 17:05:43 +0600

    generator теперь генерирует не случайную задачу, а все возможные

--- ChessPuzzleGenerator/Generator.py
+++ ChessPuzzleGenerator/Generator.py
@@ -2,6 +2,7 @@ import Utils
 import random
 
 def generate(puzzleOrig):
+
     origId = puzzleOrig[0]
     origFen = puzzleOrig[1]
     origMoveSet = puzzleOrig[2]
@@ -14,6 +15,9 @@ def generate(puzzleOrig):
     enemyKing = 'k' if origFen.split()[1] == 'b' else 'K'
     enemyKingColor = 'b' if enemyKing == 'k' else 'w'
     playerKingColor = 'w' if enemyKing == 'b' else 'b'
+    indexPos1 = 0
+    indexPiece1 = 0
+    indexPos2 = 0
 
     # генерация первого хода игрока
     humanPoses1 = Utils.getMovesToTarget(placement, moveSet[1][0:2], moveSet[1][2:4])
@@ -28,58 +32,60 @@ def generate(puzzleOrig):
             humanPoses1.remove(move)
             continue
 
-    humanPose1 = random.choice(humanPoses1)
-    moveSet[1] = humanPose1
-    placement = Utils.shiftPiece(placement, origMoveSet[1][0:2], moveSet[1][0:2])
+    for humanPose1 in humanPoses1:
+        indexPos1 += 1
+        moveSet[1] = humanPose1
+        placement = Utils.shiftPiece(placement, origMoveSet[1][0:2], moveSet[1][0:2])
 
-    possiblePieces1 = Utils.getPossiblePieces(placement, moveSet[1][0:2])
-    # print(possiblePieces1)
-    for piece in possiblePieces1[:]:
-        savedPlacement1 = savedPlacement
-        savedPlacement1 = Utils.shiftPiece(savedPlacement1, origMoveSet[1][0:2], moveSet[1][0:2])
-        savedPlacement1 = Utils.spawnPiece(savedPlacement1, moveSet[1][0:2], piece)
-        savedPlacement1 = Utils.makeMove(savedPlacement1, moveSet[0])
-        if Utils.isKingChecked(savedPlacement1, enemyKingColor):
-            possiblePieces1.remove(piece)
-            continue
-    humanPiece1 = random.choice(possiblePieces1)
-    placement = Utils.spawnPiece(placement, moveSet[1][0:2], humanPiece1)
+        possiblePieces1 = Utils.getPossiblePieces(placement, moveSet[1][0:2])
+        # print(possiblePieces1)
+        for piece in possiblePieces1[:]:
+            savedPlacement1 = savedPlacement
+            savedPlacement1 = Utils.shiftPiece(savedPlacement1, origMoveSet[1][0:2], moveSet[1][0:2])
+            savedPlacement1 = Utils.spawnPiece(savedPlacement1, moveSet[1][0:2], piece)
+            savedPlacement1 = Utils.makeMove(savedPlacement1, moveSet[0])
+            if Utils.isKingChecked(savedPlacement1, enemyKingColor):
+                possiblePieces1.remove(piece)
+                continue
+        for humanPiece1 in possiblePieces1:
+            indexPiece1 += 1
+            placement = Utils.spawnPiece(placement, moveSet[1][0:2], humanPiece1)
 
-    # генерация второго хода игрока
-    if origMoveSet[1][2:4] != origMoveSet[3][0:2]:
-        humanPoses2 = Utils.getMovesToTarget(placement, moveSet[3][0:2], moveSet[3][2:4])
-        # print(humanPoses2)
-        # if Utils.getPieceBySquare(placement, moveSet[2][0:2]) == enemyKing:
-        for move in humanPoses2[:]:
-            savedPlacement2 = savedPlacement
-            savedPlacement2 = Utils.shiftPiece(savedPlacement2, origMoveSet[1][0:2], moveSet[1][0:2])
-            savedPlacement2 = Utils.shiftPiece(savedPlacement2, origMoveSet[3][0:2], move[0:2])
-            if move[0:2] == moveSet[0][2:4] or move[0:2] == moveSet[2][2:4]:
-                humanPoses2.remove(move)
-                continue
-            if Utils.isKingChecked(savedPlacement2, playerKingColor):
-                humanPoses2.remove(move)
-                continue
-            savedPlacement2 = Utils.makeMove(savedPlacement2, moveSet[0])
-            if Utils.isKingChecked(savedPlacement2, enemyKingColor):
-                humanPoses2.remove(move)
-                continue
-            Utils.makeMove(savedPlacement2, moveSet[1])
-            if Utils.isKingChecked(savedPlacement2, playerKingColor):
-                humanPoses2.remove(move)
-                continue
-            Utils.makeMove(savedPlacement2, move)
-            if Utils.isKingChecked(savedPlacement2, enemyKingColor):
-                humanPoses2.remove(move)
-                continue
-        humanPose2 = random.choice(humanPoses2)
-        moveSet[3] = humanPose2
-        placement = Utils.shiftPiece(placement, origMoveSet[3][0:2], moveSet[3][0:2])
+            # генерация второго хода игрока
+            if origMoveSet[1][2:4] != origMoveSet[3][0:2]:
+                humanPoses2 = Utils.getMovesToTarget(placement, moveSet[3][0:2], moveSet[3][2:4])
+                # print(humanPoses2)
+                # if Utils.getPieceBySquare(placement, moveSet[2][0:2]) == enemyKing:
+                for move in humanPoses2[:]:
+                    savedPlacement2 = savedPlacement
+                    savedPlacement2 = Utils.shiftPiece(savedPlacement2, origMoveSet[1][0:2], moveSet[1][0:2])
+                    savedPlacement2 = Utils.shiftPiece(savedPlacement2, origMoveSet[3][0:2], move[0:2])
+                    if move[0:2] == moveSet[0][2:4] or move[0:2] == moveSet[2][2:4]:
+                        humanPoses2.remove(move)
+                        continue
+                    if Utils.isKingChecked(savedPlacement2, playerKingColor):
+                        humanPoses2.remove(move)
+                        continue
+                    savedPlacement2 = Utils.makeMove(savedPlacement2, moveSet[0])
+                    if Utils.isKingChecked(savedPlacement2, enemyKingColor):
+                        humanPoses2.remove(move)
+                        continue
+                    Utils.makeMove(savedPlacement2, moveSet[1])
+                    if Utils.isKingChecked(savedPlacement2, playerKingColor):
+                        humanPoses2.remove(move)
+                        continue
+                    Utils.makeMove(savedPlacement2, move)
+                    if Utils.isKingChecked(savedPlacement2, enemyKingColor):
+                        humanPoses2.remove(move)
+                        continue
+                for humanPose2 in humanPoses2:
+                    indexPos2 += 1
+                    moveSet[3] = humanPose2
+                    placement = Utils.shiftPiece(placement, origMoveSet[3][0:2], moveSet[3][0:2])
+                    moveSet = " ".join(moveSet)
+                    id = str(origId) + '_' + str(indexPos1) + '_' + str(indexPiece1) + '_' + str(indexPos2)
+                    newPuzzle = [id, placement + " " + origFen.split()[1], moveSet]
 
-    moveSet = " ".join(moveSet)
-    id = origId
-    newPuzzle = [id, placement + " " + origFen.split()[1], moveSet]
-    return newPuzzle
 
 def main():
     puzzleOrig = ['ZI07y', 'Q7/3qppkp/3p1np1/2pP4/3nP2P/1rN1B1P1/4BPK1/8 b - - 2 27', 'b3c3 e3h6 g7h6 a8f8']
